fd76ca8501328593fc9678588998f937a5b7b652
SynchronicAnalytics <DavidSchwab@mail.com>
2017-02-23 13:56:54 -0500

    Update StockDownloadScript.sh
    Added comments and ability to save downloaded files in a separate directory.

--- StockDownloadScript.sh
+++ StockDownloadScript.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
+# StockDownloadScript.sh
+# Downloads stock price data from Yahoo! for later analysis
+
+# Syntax: ./StockDownloadScript.sh <StockList> <Download Directory>
+# <StockList> is a text file with each stock symbol to be downloaded listed on a seperate line
+# <Download Directory> is relative to the current directory, and defaults to the current directory if not specified
+
+# Example: ./StockDownloadScript.sh SandPStockList StockData
+
 filename="$1"
+
+if [ -z "$2" ]
+    then directory="."
+else
+    directory="$2"
+fi
+
 while read -r symbol
 do
-    wget "http://chart.finance.yahoo.com/table.csv?s=$symbol"
+    wget "http://chart.finance.yahoo.com/table.csv?s=$symbol" -P "$directory"
 done < "$filename"
