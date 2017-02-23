8c75cba8ac02a0edcf6c482dea8fe31dbc9564b7
SynchronicAnalytics <DavidSchwab@mail.com>
2017-02-23 15:22:17 -0500

    Update RenameStockFiles.sh
    Added comments and ability to specify a target directory.

--- RenameStockFiles.sh
+++ RenameStockFiles.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
-for file in table.csv?s=*; do
+# RenameStockFiles.sh
+# Removes the "table.csv?s=" prefix from stock data downloaded with StockDownloadScript.sh
+
+# Syntax: ./RenameStockFiles.sh <Directory>
+# <Directory> is where the files are stored relative to the current directory, and defaults to the current directory
+
+# Example: ./RenameStockFiles.sh StockData
+
+if [ -z "$1" ]
+    then directory="."
+else
+    directory="$1"
+fi
+
+for file in "$directory"/table.csv?s=*; do
     mv "$file" "${file/table.csv?s=/}"
 done
